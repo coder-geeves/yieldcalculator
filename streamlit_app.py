9b008e2ed05368a4343fd65b5ceec873b286c7b2
coder-geeves <83114202+coder-geeves@users.noreply.github.com>
2024-06-13 21:13:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,10 @@ propVal = st.number_input("Property Value", 100000, 3000000, 250000, 5000)
 st.write(f'{rpm * 12 / propVal:.2%}')
 
 
-hide_st_style = """
+hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: hidden;}
             </style>
             """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
