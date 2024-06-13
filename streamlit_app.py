e50b860713a3fa4054474bdccaa973faafa3b9b9
coder-geeves <83114202+coder-geeves@users.noreply.github.com>
2024-06-13 21:11:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,17 @@ rpm = st.number_input("Rent Per Month", 0, 10000, 1000, 10)
 propVal = st.number_input("Property Value", 100000, 3000000, 250000, 5000)
 
 st.write(f'{rpm * 12 / propVal:.2%}')
+
+"""
+
+Hello
+"""
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
